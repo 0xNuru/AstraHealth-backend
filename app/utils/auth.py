ac17915500a0cd63a5dcb16315e56c0c7f982f57
nuru <0xnuru@gmail.com>
2024-05-13 12:13:33 +0100

    add documentation to auth

--- app/utils/auth.py
+++ app/utils/auth.py
@@ -18,16 +18,64 @@ oauth2_scheme = OAuth2PasswordBearerWithCookie(tokenUrl="/v1/auth/token")
 
 
 def get_password_hash(password: str) -> str:
+    """
+    Generate a hashed password using the bcrypt algorithm.
+
+    Parameters:
+    password (str): The plain text password to be hashed.
+
+    Returns:
+    str: The hashed password.
+
+    Raises:
+    None
+
+    Note:
+    This function uses the CryptContext class from the passlib library to hash the password.
+    The hashed password is returned as a string.
+    """
     hash: str = pwd_context.hash(password)
     return hash
 
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
+    """
+    Verify a plain text password against a hashed password using the bcrypt algorithm.
+
+    Parameters:
+    plain_password (str): The plain text password to be verified.
+    hashed_password (str): The hashed password to be compared with the plain text password.
+
+    Returns:
+    bool: True if the plain text password matches the hashed password, False otherwise.
+
+    Raises:
+    None
+
+    Note:
+    This function uses the CryptContext class from the passlib library to verify the password.
+    The pwd_context object is assumed to be globally defined and initialized with the bcrypt scheme.
+    """
     verified: bool = pwd_context.verify(plain_password, hashed_password)
     return verified
 
 
 def create_access_token(data: dict, expires_delta: timedelta | None = None):
+    """
+    Generate an access token for a user using JWT.
+
+    Parameters:
+    data (dict): A dictionary containing the user's information to be encoded in the JWT.
+    expires_delta (timedelta, optional): The duration for which the token should be valid. If not provided, the token will expire in 15 minutes.
+
+    Returns:
+    str: The generated access token.
+
+    Note:
+    This function uses the 'jose` lib to encode the user's information into a JWT.
+    The JWT is signed using the secret key and the specified algorithm.
+    The expiration time of the token is set based on the provided expires_delta parameter.
+    """
     to_encode = data.copy()
     if expires_delta:
         expire = datetime.now(timezone.utc) + expires_delta
@@ -41,6 +89,23 @@ def create_access_token(data: dict, expires_delta: timedelta | None = None):
 
 
 def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(load)):
+    """
+    Retrieve the current user based on the provided access token.
+
+    Parameters:
+    token (str, optional): The access token to be used for authentication. If not provided, the function will use the token provided by the OAuth2PasswordBearerWithCookie.
+    db (Session, optional): The database session object to be used for querying the user. If not provided, the function will use the session object provided by the load function.
+
+    Returns:
+    User: The User object representing the current user.
+
+    Raises:
+    HTTPException: If the access token is not valid or the user does not exist in the database.
+
+    Note:
+    This function decodes the access token using the JWT library, retrieves the username from the payload, and queries the database to find the corresponding user.
+    If the access token is not valid or the user does not exist, an HTTPException is raised with appropriate error details.
+    """
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
         detail="Could not validate credentials",
@@ -61,7 +126,23 @@ def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(
     return user
 
 
-def authenticate_user(username: str, password: str, db: Session = Depends(load)):
+def authenticate_user(username: str, password: str, db: Session = Depends(load)) -> Union[User, bool]:
+    """
+    Authenticate a user based on their username and password.
+
+    Parameters:
+    username (str): The username of the user to be authenticated.
+    password (str): The password of the user to be authenticated.
+    db (Session, optional): The database session object to be used for querying the user. If not provided, the function will use the session object provided by the load function.
+
+    Returns:
+    Union[User, bool]: If the user is authenticated and exists in the database, the User object is returned. If the user does not exist or the password is incorrect, False is returned.
+
+    Note:
+    This function queries the database to find the user with the provided username.
+    It then verifies the password using the verify_password function.
+    If the user is authenticated, the User object is returned. Otherwise, False is returned.
+    """
     user = db.query_eng(User).filter(User.email == username).first()
     if not user:
         return False
@@ -71,6 +152,21 @@ def authenticate_user(username: str, password: str, db: Session = Depends(load))
 
 
 def set_access_cookies(token: str, response: Response):
+    """
+    Set the access token as a cookie in the response.
+
+    Parameters:
+    token (str): The access token to be stored in the cookie.
+    response (Response): The FastAPI response object to which the cookie will be added.
+
+    Returns:
+    None
+
+    Note:
+    If the domain is set as the naked domain, the cookie will be available to all the
+    subdomains too. health.astrafort.tech can now have access to this cookie in the application.
+    The httponly flag is set to True to prevent client-side scripting from accessing the cookie.
+    """
     response.set_cookie(
         key="access_token",
         value=f"Bearer {token}",
@@ -85,19 +181,55 @@ def set_access_cookies(token: str, response: Response):
     )
 
 def delete_access_cookies(response: Response):
+    """
+    Delete the access token cookie from the response.
+
+    Parameters:
+    response (Response): The FastAPI response object from which the cookie will be removed.
+
+    Returns:
+    None
+
+    Note:
+    This function sets the access token cookie with an empty value and appropriate cookie attributes.
+    The cookie will be deleted from the client's browser when it expires.
+    The domain is set to "astrafort.tech" to ensure that the cookie is accessible across all subdomains.
+    The secure flag is set to True to ensure that the cookie is only transmitted over HTTPS.
+    The httponly flag is set to True to prevent client-side scripting from accessing the cookie.
+    The samesite attribute is set to "none" to allow cross-site requests to include the cookie.
+    """
     response.set_cookie(
         key="access_token",
+        value="",
         path="/",
         domain="astrafort.tech",
         secure=True,
         httponly=True,
         samesite="none",
+        expires=0,  # Set the cookie to expire immediately
     )
 
 
 def get_current_user_from_cookie(request: Request, db: Session = Depends(load)) -> User:
+    """
+    Retrieve the current user based on the access token stored in the request cookies.
+
+    Parameters:
+    request (Request): The FastAPI request object containing the cookies.
+    db (Session, optional): The database session object to be used for querying the user. If not provided, the function will use the session object provided by the load function.
+
+    Returns:
+    User: The User object representing the current user.
+
+    Raises:
+    HTTPException: If the access token is not valid or the user does not exist in the database.
+
+    Note:
+    This function retrieves the access token from the request cookies.
+    It then decodes the token using the JWT library, retrieves the username from the payload, and queries the database to find the corresponding user.
+    If the access token is not valid or the user does not exist, an HTTPException is raised with appropriate error details.
+    """
     token = request.cookies.get("access_token")
-    print(token)
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
         detail="Could not validate credentials",
